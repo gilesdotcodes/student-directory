6c66f28d38729ab319c537b985d5eb20b5648a9a
Stephen Giles <s.giles@hotmail.co.uk>
2014-09-05 11:36:38 +0100

    Exercise 8. Cohort requested with spell check.

--- directory.rb
+++ directory.rb
@@ -1,5 +1,5 @@
 def input_students
-	print "\nPlease enter the names of the students\n"
+	print "\nPlease enter the name of the first student and follow the prompts\n"
 	print "To finish, just hit return twice\n"
 	# create empty array
 	students = []
@@ -10,14 +10,30 @@ def input_students
 		# ask for their favourite hobby
 		print "Now enter #{name}'s favourite hobby\n"
 		hobby = gets.chomp
+		# ask for their cohort
+		print "Now enter #{name}'s cohort\n"
+		cohort = gets.chomp.capitalize
+		cohort=="" ? cohort=:September : ""
+		# check spelling!
+		cohort = spelling(cohort)
 		# add the student hash to the array
-		students << {:name => name, :hobby => hobby, :cohort => :September}
+		students << {:name => name.capitalize, :hobby => hobby.capitalize, :cohort => cohort.to_sym}
 		print "Now we have #{students.length} students\n"
 		# get another name from the user
 		name = gets.chomp
 	end
 	# return array of students
 	students
+end
+
+def spelling(cohort_check)
+	if cohort_check == "January" || cohort_check == "February" || cohort_check == "March" || cohort_check == "April" || cohort_check == "May" || cohort_check == "June" || cohort_check == "July" || cohort_check == "August" || cohort_check == "September" || cohort_check == "October" || cohort_check == "November" || cohort_check == "December" || cohort_check ==:September
+		return cohort_check
+	else
+		print "Please check your spelling and re-enter the cohort\n"
+		cohort_again = gets.chomp.capitalize
+		return spelling(cohort_again)
+	end
 end
 
 def print_header
