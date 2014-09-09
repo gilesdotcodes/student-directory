712e00d35610915c1fb2ba3593c206be8f3cb9a2
Stephen Giles <s.giles@hotmail.co.uk>
2014-09-09 22:49:07 +0100

    Version 10 with refactoring 1, 2, 4 and 5.

--- directory.rb
+++ directory.rb
@@ -1,7 +1,7 @@
 # declare as a global variable
 @students =[]
 
-# method to allow user to choose their action
+# method to allow user to choose their action using infinite loop
 def interactive_menu
 	loop do
 		print_menu
@@ -9,6 +9,7 @@ def interactive_menu
 	end
 end
 
+# ronseal method
 def print_menu
 	puts "Please make a selection from the menu by typing the appropriate number:"
 	puts "1. Input the students."
@@ -18,6 +19,7 @@ def print_menu
 	puts "9. Exit."
 end
 
+# method to run other methods based on user input
 def process(selection)
 	case selection
 			when 1
@@ -35,35 +37,47 @@ def process(selection)
 	end
 end
 
+# ronseal method
 def input_students
 	print "\nPlease enter the name of the first student and follow the prompts\n"
 	print "To finish, just hit return twice\n"
 	# get the first name
-	name = STDIN.gets.chomp
+	name = get_input
 	# while the name is not empty, repeat this code
 	while !name.empty? do
 		# ask for their favourite hobby
 		print "Now enter #{name}'s favourite hobby\n"
-		hobby = STDIN.gets.chomp
+		hobby = get_input
 		# ask for their cohort
 		print "Now enter #{name}'s cohort\n"
-		cohort = STDIN.gets.chomp.capitalize
+		cohort = get_input
 		#set default if they leave it blank
 		cohort=="" ? cohort=:September : ""
 		# check spelling!
 		cohort = spelling(cohort)
 		# add the student hash to the array
-		@students << {:name => name.capitalize, :hobby => hobby.capitalize, :cohort => cohort.to_sym}
+		put_student_into_array(name, hobby, cohort)
 		print @students.length==1 ? "Now we have 1 student\n" : "Now we have #{@students.length} students\n"
 		# get another name from the user
 		print "Enter the name of the next student\n"
 		print "Note: to finish, just hit return twice\n"
-		name = STDIN.gets.chomp
+		name = get_input
 	end
 	# return array of students
 	@students
 end
 
+# methods to improve the code --> D.R.Y.
+def get_input
+	input_data = STDIN.gets.chomp.capitalize
+	return input_data
+end
+
+def put_student_into_array(name, hobby, cohort)
+	@students << {:name => name, :hobby => hobby, :cohort => cohort.to_sym}
+end
+
+# 4 ronseal methods
 def show_students
 	print_header
 	print_students_list
@@ -103,12 +117,13 @@ def print_footer
 	print "\n\n"
 end
 
-def spelling(cohort_check)
-	if cohort_check == "January" || cohort_check == "February" || cohort_check == "March" || cohort_check == "April" || cohort_check == "May" || cohort_check == "June" || cohort_check == "July" || cohort_check == "August" || cohort_check == "September" || cohort_check == "October" || cohort_check == "November" || cohort_check == "December" || cohort_check ==:September
-		return cohort_check
+# method to check the spelling of the cohort
+def spelling(cohort_to_check)
+	if cohort_to_check == "January" || cohort_to_check == "February" || cohort_to_check == "March" || cohort_to_check == "April" || cohort_to_check == "May" || cohort_to_check == "June" || cohort_to_check == "July" || cohort_to_check == "August" || cohort_to_check == "September" || cohort_to_check == "October" || cohort_to_check == "November" || cohort_to_check == "December" || cohort_to_check ==:September
+		return cohort_to_check
 	else
-		print "Please check your spelling and re-enter the cohort\n"
-		cohort_again = STDIN.gets.chomp.capitalize
+		print "Please check your spelling and re-enter the cohort (month only)\n"
+		cohort_again = get_input
 		# again set default if they leave blank
 		cohort_again=="" ? cohort_again=:September : ""
 		return spelling(cohort_again)
@@ -122,28 +137,28 @@ def sort_into_cohort
 		cohort_list << student[:cohort]
 	end
 	# returns the array of cohorts but removes duplicates
-	return cohort_list.uniq
+	return cohort_list.uniq.sort
 end
 
 def save_students
 	# open the file for writing (must do this)
-	file = File.open("students.csv", "w")
+	File.open("students.csv", "w") do | file |
 	# iterate over the arrat of students
-	@students.each do |student|
-		student_data = [student[:name], student[:cohort], student[:hobby]]
-		csv_line = student_data.join(",")
-		file.puts csv_line
+		@students.each do |student|
+			student_data = [student[:name], student[:cohort], student[:hobby]]
+			csv_line = student_data.join(",")
+			file.puts csv_line
+		end
 	end
-	file.close
 end
 
 def load_students (filename = "students.csv")
-	file = File.open(filename, "r")
-	file.readlines.each do | line |
-		name, cohort, hobby = line.chomp.split(',')
-			@students << {:name => name, :cohort => cohort.to_sym, :hobby => hobby}
+	File.open(filename, "r") do | file |
+	    file.readlines.each do | line |
+			name, cohort, hobby = line.chomp.split(',')
+			put_student_into_array(name, hobby, cohort)
+		end
 	end
-	file.close
 end
 
 def try_load_students
